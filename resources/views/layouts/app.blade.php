ee5052de2f5467754292f9a29b2934be720620b0
pktzj117 <416335127@qq.com>
2020-07-17 13:49:45 +0800

    基础页面构造

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <mate charset="utf-8">
+  <mate http-equiv="X-UA-Compatible" content="IE=edge">
+  <mate name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <mate name="csrf-token" content="{{ csrf_token() }}">
+  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+  <div id="app" class="{{ route_class() }}-page">
+
+    @include('layouts._header')
+
+    <div class="container">
+
+      @include('shared._messages')
+
+      @yield('content')
+
+    </div>
+
+    @include('layouts._footer')
+  </div>
+
+  <!-- Scripts -->
+  <script type="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
