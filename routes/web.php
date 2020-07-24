a4e2283ae62fa3485e7ea15ddb7d8042e7ba3be3
pktzj117 <416335127@qq.com>
2020-07-24 14:09:01 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Auth::routes(['verify' => true]);
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
