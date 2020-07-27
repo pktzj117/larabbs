7161c70f531a0110785e400e6fb145bfa9bb75a2
pktzj117 <416335127@qq.com>
2020-07-27 15:07:15 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -18,4 +18,35 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+
+        //不同的排序,是哟个不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        //预加载防止 N+1问题
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        //当话题又新回复时, 我们将编写逻辑更新话题模型的 reply_cout 属性
+        //此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 安装创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
 }
