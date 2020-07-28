c9eb3a71025f11566401280284f58694aec58826
pktzj117 <416335127@qq.com>
2020-07-28 16:40:02 +0800

    用户可以上传话题图片

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
-
+  @yield('styles')
 </head>
 <body>
   <div id="app" class="{{ route_class() }}-page">
@@ -31,6 +31,7 @@
 
   <!-- Scripts -->
   <script src="{{ mix('js/app.js') }}"></script>
+  @yield('scripts')
 </body>
 
 </html>
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -42,7 +42,7 @@
           </div>
 
           <div class="form-group">
-          	<textarea name="body" id="body-field" class="form-control" placeholder="请填入至少三个字符" rows="6">{{ old('body', $topic->body ) }}</textarea>
+          	<textarea name="body" id="editor" class="form-control" placeholder="请填入至少三个字符" rows="6">{{ old('body', $topic->body ) }}</textarea>
           </div>
 
 
@@ -57,3 +57,32 @@
 </div>
 
 @endsection
+
+@section('styles')
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+  <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+
+  <script >
+    $(document).ready(function() {
+      var editor = new Simditor({
+        textarea: $('#editor'),
+        upload: {
+          url: '{{ route('topics.upload_image') }}',
+          params:{
+            _token: '{{ csrf_token() }}'
+          },
+          fileKey: 'upload_file',
+          connectionCount: 3,
+          leaveConfirm: '文件上传中, 关闭此页面将取消上传.'
+        },
+        pasteImage: true,
+      });
+    });
+  </script>
+@stop
